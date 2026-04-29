dae58277aea277c2d6fddfa318a488e1b0b4a74f
Mykyta Firsov <nik.firs1070@gmail.com>
2026-04-29 17:10:54 +0300

    deleted second replace

--- src/app/Corrector.java
+++ src/app/Corrector.java
@@ -8,7 +8,7 @@ public class Corrector {
         for (int i = 0; i < strs.length; i++) {
             String str = strs[i];
             if (str != null && str.contains("b")) {
-                str = str.replace("b", "o").replace("B", "O");
+                str = str.replace("b", "o");
             }
 
             stringBuilder.append(count++)
