fb2155fcb8a99efef08e3d00585e75c146766530
Mykyta Firsov <nik.firs1070@gmail.com>
2026-04-29 16:47:26 +0300

    code fix

--- src/app/Corrector.java
+++ src/app/Corrector.java
@@ -5,15 +5,19 @@ public class Corrector {
     public String handleData(String[] strs) {
         StringBuilder stringBuilder = new StringBuilder();
         int count = 1;
-        for (String str : strs) {
-            if ( str != null && str.contains("b")) {
+        for (int i = 0; i < strs.length; i++) {
+            String str = strs[i];
+            if (str != null && str.contains("b")) {
                 str = str.replace("b", "o").replace("B", "O");
             }
 
             stringBuilder.append(count++)
                     .append(") ")
-                    .append(str)
-                    .append("\n");
+                    .append(str);
+
+            if (i < strs.length - 1) {
+                stringBuilder.append("\n");
+            }
         }
         return stringBuilder.toString();
     }
