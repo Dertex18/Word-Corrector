001412e9c74f8422c77c07308fd8ea6f3f4b620b
Mykyta Firsov <nik.firs1070@gmail.com>
2026-04-29 16:52:09 +0300

    .

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,7 +3,7 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
-        getOutput(Corrector().handleData(
+        getOutput(new Corrector().handleData(
                 new DataProvider())
         );
     }
